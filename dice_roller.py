c34817f8909e5de6bf0c75124b620d45e46dbddf
Anthony Rhodes <starcaster@cyberrhodes.com>
2021-04-08 09:56:09 -0700

    Added functionality for loop to base script

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,21 @@
+import random
+
+
 def main():
+  dice_sum = 0   
+  #roll = 5 
+  dice_rolls = 2
+  for i in range(0,dice_rolls):
+
+    roll = random.randint(1,6)  
+    dice_sum += roll 
+    print(f'the number rolled: {roll}')
+  print(f'The sum of your rolls: {dice_sum}')    
 
   #print('You rolled a die')
-  print('"The die has been cast, now we shall see who laughs last!"')
-
+  print('"========================================"')
+  print(dice_rolls)
+  print(roll)
 
 if __name__== "__main__":
   main()
